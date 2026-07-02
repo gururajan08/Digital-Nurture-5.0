9b518bb3941ffd05d57664e3239cdfe482b02c1e
gururajan08 <145822402+gururajan08@users.noreply.github.com>
2026-07-02 19:32:52 +0530

    Update SingletonPatternExample.java

--- Design Patterns and Principles/SingletonPatternExample.java
+++ Design Patterns and Principles/SingletonPatternExample.java
@@ -1,1 +1,40 @@
+public class SingletonPatternExample {
 
+    static class Logger {
+
+        private static Logger instance;
+
+        private Logger() {
+            System.out.println("Logger Instance Created");
+        }
+
+        public static Logger getInstance() {
+            if (instance == null) {
+                instance = new Logger();
+            }
+            return instance;
+        }
+
+        public void log(String message) {
+            System.out.println("LOG: " + message);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        logger1.log("First log message.");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("Second log message.");
+
+        if (logger1 == logger2) {
+            System.out.println("\nOnly one Logger instance exists.");
+        } else {
+            System.out.println("\nMultiple Logger instances exist.");
+        }
+
+        System.out.println("Logger1 HashCode: " + logger1.hashCode());
+        System.out.println("Logger2 HashCode: " + logger2.hashCode());
+    }
+}
